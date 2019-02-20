fe34ff548bd934d2962d800d92bf02dfa3052af8
JuanR <juan.restrepo81@udea.edu.co>
2019-02-19 22:12:57 -0500

    emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'hola@example.com',
-        text: 'Hey. this is hola'
-    });
-
 });
 
 socket.on('disconnect', function() {
