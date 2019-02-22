7975cb947428867d686ddfffb13529b338d81e87
JuanR <juan.restrepo81@udea.edu.co>
2019-02-21 21:47:42 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket = io();
+
+function scrollToBottom() {
+    var messages = jQuery('#messages');
+
+    var newMessage = messages.children('li:last-child')
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        console.log('should scroll');
+    }
+
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
@@ -16,6 +34,7 @@ socket.on('newMessage', function (message) {
         createdAt: formatedTime
     });
     jQuery("#messages").append(html);
+    scrollToBottom();
 
 
 });
@@ -29,6 +48,7 @@ socket.on('newLocationMessage', function(message) {
         url: message.url
     })
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 });
 
